fe6907ef3ab8f3768c089683faba4d18ecf68a02
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-08-31 18:42:28 +0200

    ADDed staff popup

---
+++ db-crociere/db-crociere/Staff/AddStaffPopup.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace db_crociere.Staff
+{
+    partial class AddStaffPopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel10 = new System.Windows.Forms.FlowLayoutPanel();
+            this.InsertCabinCheckBox = new System.Windows.Forms.CheckBox();
+            this.InsertRoomCheckBox = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel9 = new System.Windows.Forms.FlowLayoutPanel();
+            this.FiscalCodeInfo = new System.Windows.Forms.Label();
+            this.DeckNumberSpaceInfo = new System.Windows.Forms.Label();
+            this.SpacesNumberInfo = new System.Windows.Forms.Label();
+            this.SpacesNumberTextBox = new System.Windows.Forms.TextBox();
+            this.CapacitySpaceInfo = new System.Windows.Forms.Label();
+            this.SpaceCapacityTextBox = new System.Windows.Forms.TextBox();
+            this.PositionInfo = new System.Windows.Forms.Label();
+            this.CabinTypeInfo = new System.Windows.Forms.Label();
+            this.AddSpacesBtn = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.FiscalCodeTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox6.SuspendLayout();
+            this.flowLayoutPanel10.SuspendLayout();
+            this.flowLayoutPanel9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.flowLayoutPanel10);
+            this.groupBox6.Controls.Add(this.flowLayoutPanel9);
+            this.groupBox6.Controls.Add(this.AddSpacesBtn);
+            this.groupBox6.Controls.Add(this.button8);
+            this.groupBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox6.Location = new System.Drawing.Point(12, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(753, 187);
+            this.groupBox6.TabIndex = 25;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Inserisci Personale";
+            // 
+            // flowLayoutPanel10
+            // 
+            this.flowLayoutPanel10.Controls.Add(this.InsertCabinCheckBox);
+            this.flowLayoutPanel10.Controls.Add(this.InsertRoomCheckBox);
+            this.flowLayoutPanel10.Location = new System.Drawing.Point(2, 19);
+            this.flowLayoutPanel10.Name = "flowLayoutPanel10";
+            this.flowLayoutPanel10.Size = new System.Drawing.Size(126, 162);
+            this.flowLayoutPanel10.TabIndex = 15;
+            // 
+            // InsertCabinCheckBox
+            // 
+            this.InsertCabinCheckBox.AutoSize = true;
+            this.InsertCabinCheckBox.Location = new System.Drawing.Point(3, 3);
+            this.InsertCabinCheckBox.Name = "InsertCabinCheckBox";
+            this.InsertCabinCheckBox.Size = new System.Drawing.Size(97, 20);
+            this.InsertCabinCheckBox.TabIndex = 30;
+            this.InsertCabinCheckBox.Text = "Equipaggio";
+            this.InsertCabinCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // InsertRoomCheckBox
+            // 
+            this.InsertRoomCheckBox.AutoSize = true;
+            this.InsertRoomCheckBox.Location = new System.Drawing.Point(3, 29);
+            this.InsertRoomCheckBox.Name = "InsertRoomCheckBox";
+            this.InsertRoomCheckBox.Size = new System.Drawing.Size(60, 20);
+            this.InsertRoomCheckBox.TabIndex = 31;
+            this.InsertRoomCheckBox.Text = "Civile";
+            this.InsertRoomCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel9
+            // 
+            this.flowLayoutPanel9.Controls.Add(this.FiscalCodeInfo);
+            this.flowLayoutPanel9.Controls.Add(this.FiscalCodeTextBox);
+            this.flowLayoutPanel9.Controls.Add(this.DeckNumberSpaceInfo);
+            this.flowLayoutPanel9.Controls.Add(this.textBox1);
+            this.flowLayoutPanel9.Controls.Add(this.SpacesNumberInfo);
+            this.flowLayoutPanel9.Controls.Add(this.SpacesNumberTextBox);
+            this.flowLayoutPanel9.Controls.Add(this.CapacitySpaceInfo);
+            this.flowLayoutPanel9.Controls.Add(this.SpaceCapacityTextBox);
+            this.flowLayoutPanel9.Controls.Add(this.PositionInfo);
+            this.flowLayoutPanel9.Controls.Add(this.CabinTypeInfo);
+            this.flowLayoutPanel9.Controls.Add(this.label1);
+            this.flowLayoutPanel9.Controls.Add(this.label2);
+            this.flowLayoutPanel9.Controls.Add(this.label3);
+            this.flowLayoutPanel9.Controls.Add(this.label4);
+            this.flowLayoutPanel9.Location = new System.Drawing.Point(134, 19);
+            this.flowLayoutPanel9.Name = "flowLayoutPanel9";
+            this.flowLayoutPanel9.Size = new System.Drawing.Size(524, 162);
+            this.flowLayoutPanel9.TabIndex = 14;
+            // 
+            // FiscalCodeInfo
+            // 
+            this.FiscalCodeInfo.AutoSize = true;
+            this.FiscalCodeInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiscalCodeInfo.Location = new System.Drawing.Point(3, 3);
+            this.FiscalCodeInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.FiscalCodeInfo.Name = "FiscalCodeInfo";
+            this.FiscalCodeInfo.Size = new System.Drawing.Size(98, 16);
+            this.FiscalCodeInfo.TabIndex = 28;
+            this.FiscalCodeInfo.Text = "Codice Fiscale";
+            // 
+            // DeckNumberSpaceInfo
+            // 
+            this.DeckNumberSpaceInfo.AutoSize = true;
+            this.DeckNumberSpaceInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeckNumberSpaceInfo.Location = new System.Drawing.Point(259, 3);
+            this.DeckNumberSpaceInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.DeckNumberSpaceInfo.Name = "DeckNumberSpaceInfo";
+            this.DeckNumberSpaceInfo.Size = new System.Drawing.Size(45, 16);
+            this.DeckNumberSpaceInfo.TabIndex = 30;
+            this.DeckNumberSpaceInfo.Text = "Nome";
+            // 
+            // SpacesNumberInfo
+            // 
+            this.SpacesNumberInfo.AutoSize = true;
+            this.SpacesNumberInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpacesNumberInfo.Location = new System.Drawing.Point(3, 31);
+            this.SpacesNumberInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.SpacesNumberInfo.Name = "SpacesNumberInfo";
+            this.SpacesNumberInfo.Size = new System.Drawing.Size(67, 16);
+            this.SpacesNumberInfo.TabIndex = 32;
+            this.SpacesNumberInfo.Text = "Cognome";
+            // 
+            // SpacesNumberTextBox
+            // 
+            this.SpacesNumberTextBox.Location = new System.Drawing.Point(76, 31);
+            this.SpacesNumberTextBox.MaxLength = 40;
+            this.SpacesNumberTextBox.Name = "SpacesNumberTextBox";
+            this.SpacesNumberTextBox.Size = new System.Drawing.Size(177, 22);
+            this.SpacesNumberTextBox.TabIndex = 33;
+            // 
+            // CapacitySpaceInfo
+            // 
+            this.CapacitySpaceInfo.AutoSize = true;
+            this.CapacitySpaceInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CapacitySpaceInfo.Location = new System.Drawing.Point(259, 31);
+            this.CapacitySpaceInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.CapacitySpaceInfo.Name = "CapacitySpaceInfo";
+            this.CapacitySpaceInfo.Size = new System.Drawing.Size(75, 16);
+            this.CapacitySpaceInfo.TabIndex = 34;
+            this.CapacitySpaceInfo.Text = "Nazionalità";
+            // 
+            // SpaceCapacityTextBox
+            // 
+            this.SpaceCapacityTextBox.Enabled = false;
+            this.SpaceCapacityTextBox.Location = new System.Drawing.Point(340, 31);
+            this.SpaceCapacityTextBox.MaxLength = 3;
+            this.SpaceCapacityTextBox.Name = "SpaceCapacityTextBox";
+            this.SpaceCapacityTextBox.Size = new System.Drawing.Size(47, 22);
+            this.SpaceCapacityTextBox.TabIndex = 35;
+            // 
+            // PositionInfo
+            // 
+            this.PositionInfo.AutoSize = true;
+            this.PositionInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PositionInfo.Location = new System.Drawing.Point(393, 31);
+            this.PositionInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.PositionInfo.Name = "PositionInfo";
+            this.PositionInfo.Size = new System.Drawing.Size(78, 16);
+            this.PositionInfo.TabIndex = 36;
+            this.PositionInfo.Text = "Passaporto";
+            // 
+            // CabinTypeInfo
+            // 
+            this.CabinTypeInfo.AutoSize = true;
+            this.CabinTypeInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CabinTypeInfo.Location = new System.Drawing.Point(3, 59);
+            this.CabinTypeInfo.Margin = new System.Windows.Forms.Padding(3);
+            this.CabinTypeInfo.Name = "CabinTypeInfo";
+            this.CabinTypeInfo.Size = new System.Drawing.Size(63, 16);
+            this.CabinTypeInfo.TabIndex = 38;
+            this.CabinTypeInfo.Text = "Recapito";
+            // 
+            // AddSpacesBtn
+            // 
+            this.AddSpacesBtn.Location = new System.Drawing.Point(664, 19);
+            this.AddSpacesBtn.Name = "AddSpacesBtn";
+            this.AddSpacesBtn.Size = new System.Drawing.Size(75, 25);
+            this.AddSpacesBtn.TabIndex = 3;
+            this.AddSpacesBtn.Text = "Aggiungi";
+            this.AddSpacesBtn.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(664, 57);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Pulisci";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // FiscalCodeTextBox
+            // 
+            this.FiscalCodeTextBox.Location = new System.Drawing.Point(107, 3);
+            this.FiscalCodeTextBox.MaxLength = 16;
+            this.FiscalCodeTextBox.Name = "FiscalCodeTextBox";
+            this.FiscalCodeTextBox.Size = new System.Drawing.Size(146, 22);
+            this.FiscalCodeTextBox.TabIndex = 40;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(310, 3);
+            this.textBox1.MaxLength = 30;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 41;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Stipendio Annuo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 16);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Anzianità di Servizio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(317, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Grado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(369, 59);
+            this.label4.Margin = new System.Windows.Forms.Padding(3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Ruolo";
+            // 
+            // AddStaffPopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 450);
+            this.Controls.Add(this.groupBox6);
+            this.Name = "AddStaffPopup";
+            this.Text = "Form1";
+            this.groupBox6.ResumeLayout(false);
+            this.flowLayoutPanel10.ResumeLayout(false);
+            this.flowLayoutPanel10.PerformLayout();
+            this.flowLayoutPanel9.ResumeLayout(false);
+            this.flowLayoutPanel9.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel10;
+        private System.Windows.Forms.CheckBox InsertCabinCheckBox;
+        private System.Windows.Forms.CheckBox InsertRoomCheckBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel9;
+        private System.Windows.Forms.Label FiscalCodeInfo;
+        private System.Windows.Forms.Label DeckNumberSpaceInfo;
+        private System.Windows.Forms.Label SpacesNumberInfo;
+        private System.Windows.Forms.TextBox SpacesNumberTextBox;
+        private System.Windows.Forms.Label CapacitySpaceInfo;
+        private System.Windows.Forms.TextBox SpaceCapacityTextBox;
+        private System.Windows.Forms.Label PositionInfo;
+        private System.Windows.Forms.Label CabinTypeInfo;
+        private System.Windows.Forms.Button AddSpacesBtn;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox FiscalCodeTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ db-crociere/db-crociere/Staff/AddStaffPopup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace db_crociere.Staff
+{
+    public partial class AddStaffPopup : Form
+    {
+
+        private DataClassesDBCrociereDataContext db;
+
+        public AddStaffPopup(DataClassesDBCrociereDataContext db)
+        {
+            this.db = db;
+            InitializeComponent();
+        }
+    }
+}
